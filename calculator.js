0794e3d145e534ea8f738e2646d989531c05fcf1
Anisha Lende <105670034+AnishaLende@users.noreply.github.com>
2023-01-30 13:37:17 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,27 @@
+let string="";
+let buttons=document.querySelectorAll(".button");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        if(e.target.innerHTML=="="){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            document.querySelector(".input").value=string;
+        }
+        else if(e.target.innerHTML=="M+"){
+            document.querySelector('.no').value="";
+        }
+        else if(e.target.innerHTML=="M-"){
+            document.querySelector('.no').value="";
+        }
+        else if(e.target.innerHTML=="X"){
+            document.querySelector('.no').value="";
+        }
+        else{
+            string=string + e.target.innerHTML;
+            document.querySelector("input").value=string;
+        }
+    })
+})
